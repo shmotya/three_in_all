53630dbb1446c395eff655b7ae85cac4ab07d12c
shmotya <shvydkodan@gmail.com>
2023-03-19 17:04:46 +0200

    anecdotes ui added

--- app/src/main/java/com/example/all_by_three/AnecdotesActivity.kt
+++ app/src/main/java/com/example/all_by_three/AnecdotesActivity.kt
@@ -1,11 +1,23 @@
 package com.example.all_by_three
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.all_by_three.databinding.ActivityAnecdotesBinding
+import com.example.all_by_three.databinding.ActivityMainBinding
 
 class AnecdotesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnecdotesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_anecdotes)
+        binding = ActivityAnecdotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibBackAnecdotes.setOnClickListener {
+            val intent = Intent(this, AnecdotesActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/all_by_three/ImageActivity.kt
+++ app/src/main/java/com/example/all_by_three/ImageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.all_by_three
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.all_by_three.databinding.ActivityImageBinding
@@ -10,14 +11,14 @@ class ImageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
         super.onCreate(savedInstanceState)
         binding = ActivityImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.ibBackImages.setOnClickListener {
+            val intent = Intent(this, ImageActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
